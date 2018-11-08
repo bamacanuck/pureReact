84e99dd2a606718e494a0e8866bde86848918e53
Shane Scott <shaneradio@gmail.com>
2018-11-08 18:20:40 -0500

    the 'hello' dir... src/index.js...

--- hello/react-hello/src/index.js
+++ hello/react-hello/src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from "react-dom";
 function HelloWorld () {
     return (
         <div>hello, world</div>
-    )
+    );
 }
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.querySelector("#root")
+);
