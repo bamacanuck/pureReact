99bd6903e259550aae0912a82d8d85547e1a43da
Shane Scott <shaneradio@gmail.com>
2018-11-09 22:27:22 -0500

    alternate React function

--- hello/react-hello/src/index.js
+++ hello/react-hello/src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function HelloWorld () {
-    return (
-        <div>hello, world...</div>
-    );
-}
-
 // function HelloWorld () {
-//     return React.createElement (
-//         'div',
-//         {},
-//         "hello, world, and you..."
+//     return (
+//         <div>hello, world... ...</div>
 //     );
 // }
+
+function HelloWorld () {
+    return React.createElement (
+        'div',
+        {},
+        "hello, world, and you..."
+    );
+}
 
 ReactDOM.render(
     <HelloWorld/>,
