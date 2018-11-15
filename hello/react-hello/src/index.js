9704873ff69d5180e1b6a63543e1208d12429464
Shane Scott <shaneradio@gmail.com>
2018-11-15 17:00:51 -0500

    a discovery with JSX...

--- hello/react-hello/src/index.js
+++ hello/react-hello/src/index.js
@@ -34,42 +34,54 @@ import ReactDOM from "react-dom";
 //     );
 // }
 
+// function HelloWorld () {
+//     return React.createElement (
+//         'div',
+//         {},
+//         "a",
+//         React.createElement (
+//             'div',
+//             {},
+//             <br/>,
+//             React.createElement (
+//                 'div',
+//                 {},
+//                 "b",
+//                 React.createElement (
+//                     'div',
+//                     {},
+//                     <br/>,
+//                     React.createElement (
+//                         'div',
+//                         {},
+//                         "c",
+//                         React.createElement (
+//                             'div',
+//                             {},
+//                             <br/>,
+//                             React.createElement (
+//                                 'div',
+//                                 {},
+//                                 "hey now"
+//                             )
+//                         )
+//                     )
+//                 )
+//             )
+//         )
+//     )
+// }
+
 function HelloWorld () {
-    return React.createElement (
-        'div',
-        {},
-        "a",
-        React.createElement (
-            'div',
-            {},
-            <br/>,
-            React.createElement (
-                'div',
-                {},
-                "b",
-                React.createElement (
-                    'div',
-                    {},
-                    <br/>,
-                    React.createElement (
-                        'div',
-                        {},
-                        "c",
-                        React.createElement (
-                            'div',
-                            {},
-                            <br/>,
-                            React.createElement (
-                                'div',
-                                {},
-                                "hey now"
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
+    return (
+        <div>a</div>
+        // <br/>,
+        // <div>b</div>
+        // <br/>,
+        // <div>c</div>
+        // <br/>
+        // <div>hey now</div>
+    );
 }
 
 
